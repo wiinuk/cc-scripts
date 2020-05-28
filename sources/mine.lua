d1a170852209dcca2fa20c0f10516ab8eecda107
wiinuk <wii.nukegara@gmail.com>
2020-05-28 15:54:54 +0900

    更新

--- sources/mine.lua
+++ sources/mine.lua
@@ -3,30 +3,76 @@
 
 --- ホームを (0, 0, 0) としたときの相対座標
 local position = { x = 0, y = 0, z = 0 }
+local memory = {
+    -- { dart = 10, sand = 3 }
+    blockToDigTryCount = {},
+    blockToDigSuccessCount = {},
+    -- { 0,0,0, 0,0,1, 0,1,1 }
+    moveHistory = {},
+}
+
+local function moveDown()
+    local ok, error = turtle.down()
+    if ok then
+        position.y = position.y - 1
+
+        local h = memory.moveHistory
+        h[#h+1] = position.x
+        h[#h+1] = position.y
+        h[#h+1] = position.z
+    end
+    return ok, error
+end
+
+local function digDown()
+    local ok, info = turtle.inspectDown()
+    if not ok then return false, info end
+    local blockName = info.name
+
+    memory.blockToDigTryCount[blockName] = memory.blockToDigTryCount[blockName] + 1
+    local ok, info = turtle.digDown()
+
+    if not ok then return false, info end
+
+    memory.blockToDigSuccessCount[blockName] = memory.blockToDigSuccessCount[blockName] + 1
+    return true
+end
+
+local function mineDown1()
+    if not moveDown() then
+        -- 行けなかった
+
+        -- ブロックがあるなら
+        if turtle.detectDown() then
+            -- 掘る
+            digDown()
+
+            -- 拾う
+            local ok, reason = turtle.suckDown()
+            if not ok then
+
+                -- 拾えなかった
+                error("suckDown failed: "..reason)
+            end
+        end
+
+        local ok, reason = moveDown()
+        if not ok then error("moveDown failed: "..reason) end
+    end
+end
+local function mining()
+    while true do
+        mineDown1()
+        os.sleep(0)
+    end
+end
 
 -- turtle.detectDown
 -- turtle.inspectDown
 -- turtle.attack
 
-local function moveDown()
-    local ok, error = turtle.down()
-    if ok then position.y = position.y - 1 end
-    return ok, error
-end
-
-local function mineDown()
-    while true do
-        local ok, reason = turtle.digDown()
-        if not ok then error("digDown failed: "..reason) end
-        local ok, reason = turtle.suckDown()
-        if not ok then error("suckDown failed: "..reason) end
-        local ok, reason = moveDown()
-        if not ok then error("moveDown failed: "..reason) end
-    end
-end
-
 local commands = {
-    mineDown = mineDown
+    mining = mining
 }
 
 local function processArguments(x, ...) commands[x](...) end
