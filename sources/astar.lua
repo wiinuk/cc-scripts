cb849b27fbd7f8bb8109b9a9942faa7a22cb2267
wiinuk <wii.nukegara@gmail.com>
2020-06-05 06:59:21 +0900

    key バージョン

--- sources/astar.lua
+++ sources/astar.lua
@@ -1,14 +1,6 @@
 local Heap = require "heap"
 
----@class AStarNode
----@field public x integer
----@field public y integer
----@field public z integer
----@field public status integer
----@field public cost number
----@field public hCost number
----@field public score number
----@field public parent AStarNode|nil
+---@class AStarKey : integer
 
 ---@class AStarFinder
 ---@field public goalX integer
@@ -16,15 +8,26 @@ local Heap = require "heap"
 ---@field public goalZ integer
 ---@field public isWall fun(x: integer, y: integer, z: integer): boolean
 ---@field public hCost fun(x: integer, y: integer, z: integer, goalX: integer, goalY: integer, goalZ: integer): number
----@field public opens AStarNode[] @heap
----@field public positionToNode table<integer, AStarNode>
+---@field public opens table<integer, AStarKey> @heap
 
-local None = 0
+---@field public compareNodeOfCost fun(a: AStarKey, b: AStarKey): number
+---@field public positionToStatus table<AStarKey, integer>
+---@field public positionToCost table<AStarKey, number>
+---@field public positionToScore table<AStarKey, number>
+---@field public positionToParent table<AStarKey, AStarKey>
+
 local Open = 1
 local Closed = -1
 
 local function isInt17(x)
     return -65536 <= x and x <= 65535 and math.modf(x) == x
+end
+
+---@param x integer
+---@param y integer
+---@param z integer
+local function isValidPosition(x, y, z)
+    return isInt17(x) and isInt17(y) and isInt17(z)
 end
 
 -- 3要素の整数ベクトルを1つの成分当たり 17bit づつ使って double に格納する
@@ -33,16 +36,16 @@ end
 --- -65536 <= x <= 65535
 local function positionToKey(x, y, z)
     return
-        (x + 0x10000) * 0x400000000 +
-        (y + 0x10000) * 0x20000 +
-        (z + 0x10000)
+        (x + 65536 --[[ 0x10000 ]]) * 17179869184 --[[ 0x400000000 ]] +
+        (y + 65536) * 131072 --[[ 0x20000 ]] +
+        (z + 65536)
 end
 
 --- double に格納された3要素の整数ベクトルを取り出す
 local function keyToPosition(key)
-    local x = (math.modf(key / 0x400000000) % 0x20000) - 0x10000
-    local y = (math.modf(key / 0x20000) % 0x20000) - 0x10000
-    local z = (key % 0x20000) - 0x10000
+    local x = math.modf(key / 17179869184 --[[ 0x400000000 ]]) % 131072 --[[ 0x20000 ]] - 65536 --[[ 0x10000 ]]
+    local y = math.modf(key / 131072 --[[ 0x20000 ]]) % 131072 - 65536
+    local z = key % 131072 - 65536
     return x, y, z
 end
 
@@ -50,53 +53,30 @@ local function hCost(x, y, z, goalX, goalY, goalZ)
     return (goalX - x) + (goalY - y) + (goalZ - z)
 end
 
-local function compareNodeOfCost(a, b)
-    local c = b.score - a.score
-    if c ~= 0 then return c end
-    return b.cost - a.cost
+local function printNode(self, message, key)
+    local x, y = keyToPosition(key)
+    local cost = self.positionToCost[key]
+    local score = self.positionToScore[key]
+    print(message, x, y, "c:", cost, "s:", score)
 end
+local function open(self, key, parentKey)
+    if self.positionToStatus[key] then return end
 
-local function none(self, x, y, z)
-    local key = positionToKey(x, y, z)
-    local node = self.positionToNode[key]
-    if node then return node end
+    self.positionToStatus[key] = Open
+    self.positionToParent[key] = parentKey
+    local cost = (self.positionToCost[parentKey] or 0) + 1
+    self.positionToCost[key] = cost
 
-    local node = {
-        x = x,
-        y = y,
-        z = z,
-        status = None,
-        cost = 0,
-        hCost = 0,
-        score = 0,
-        parent = nil,
-    }
-    self.positionToNode[key] = node
-    return node
-end
-
-local function printNode(message, node)
-    print(message, node.x, node.y, "c:", node.cost, "h:", node.hCost, "s:", node.score)
-end
-local function open(self, node, parent)
-    if node.status ~= None then return end
-
-    node.status = Open
-    node.parent = parent
-    if parent then
-        node.cost = parent.cost + 1
-    else
-        node.cost = 0
-    end
-    node.hCost = self.hCost(
-        node.x, node.y, node.z,
+    local x, y, z = keyToPosition(key)
+    local hCost = self.hCost(
+        x, y, z,
         self.goalX, self.goalY, self.goalZ
     )
     -- static weighing
-    node.score = node.cost + node.hCost * 5
-    Heap.push(self.opens, node, compareNodeOfCost)
+    self.positionToScore[key] = cost + hCost * 5
 
-    -- printNode("open", node)
+    Heap.push(self.opens, key, self.compareNodeOfCost)
+    -- printNode(self, "open", key)
 end
 
 local function reverse(xs)
@@ -106,13 +86,14 @@ local function reverse(xs)
     end
 end
 
-local function makePath(n)
+local function makePath(self, n)
     local path = {}
     while n do
-        path[#path+1] = n.z
-        path[#path+1] = n.y
-        path[#path+1] = n.x
-        n = n.parent
+        local x, y, z = keyToPosition(n)
+        path[#path+1] = z
+        path[#path+1] = y
+        path[#path+1] = x
+        n = self.positionToParent[n]
     end
     reverse(path)
     return path
@@ -127,34 +108,49 @@ local neighborNormals = {
 }
 local function findPath(isWall, startX, startY, startZ, goalX, goalY, goalZ)
     ---@type AStarFinder
-    local f = {
+    local self = {
         goalX = goalX,
         goalY = goalY,
         goalZ = goalZ,
         isWall = isWall,
         hCost = hCost,
         opens = {},
-        positionToNode = {},
-    }
-    open(f, none(f, startX, startY, startZ), nil)
-    while true do
-        local p = Heap.pop(f.opens, compareNodeOfCost)
-        if not p then return false end
 
-        -- printNode("pop", p)
-        if p.x == f.goalX and p.y == f.goalY and p.z == f.goalZ then return makePath(p) end
+        positionToCost = {},
+        positionToScore = {},
+        positionToStatus = {},
+        positionToParent = {},
+    }
+
+    function self.compareNodeOfCost(a, b)
+        local c = self.positionToScore[b] - self.positionToScore[a]
+        if c ~= 0 then return c end
+        return self.positionToCost[b] - self.positionToCost[a]
+    end
+
+    local startKey = positionToKey(startX, startY, startZ)
+    open(self, startKey)
+
+    while true do
+        local pk = Heap.pop(self.opens, self.compareNodeOfCost)
+        if not pk then return false end
+        local px, py, pz = keyToPosition(pk)
+
+        -- printNode(self, "pop", pk)
+        if px == self.goalX and py == self.goalY and pz == self.goalZ then return makePath(self, pk) end
 
         for i = 1, #neighborNormals, 3 do
-            local x, y, z = p.x + neighborNormals[i], p.y + neighborNormals[i+1], p.z + neighborNormals[i+2]
+            local x, y, z = px + neighborNormals[i], py + neighborNormals[i+1], pz + neighborNormals[i+2]
             if not isWall(x, y, z) then
-                open(f, none(f, x, y, z), p)
+                open(self, positionToKey(x, y, z), pk)
             end
         end
-        p.status = Closed
-        -- printNode("closed", p)
+        self.positionToStatus[pk] = Closed
+        -- printNode(self, "closed", pk)
     end
 end
 
 return {
+    isValidPosition = isValidPosition,
     findPath = findPath,
 }
