0cd3b8735aeabe7e6da0afb893b391939f2a56a3
wiinuk <wii.nukegara@gmail.com>
2020-06-07 14:52:24 +0900

    sync

--- sources/farm.lua
+++ sources/farm.lua
@@ -1,13 +1,78 @@
-local ok, item = turtle.inspectDown()
-if ok and
-    (
-        item.name == "minecraft:wheat" or
-        item.name == "minecraft:carrots"
-    )
-    and
-    (
-        7 <= item.state.age
-    )
-then
-    turtle.digDown()
+local function findItem(predicate)
+    for i = 1, 16 do
+        local item = turtle.getItemDetail(i)
+        if item and predicate(item) then return i end
+    end
+    return false
+end
+local function selectItem(predicate)
+    local slot = findItem(predicate)
+    if slot then
+        local ok, reason = turtle.select(slot)
+        if not ok then error(reason) end
+    end
+    error("slot not found")
+end
+
+local seedNames = {
+    "minecraft:carrot",
+    "minecraft:wheat_seeds",
+}
+local plantNames = {
+    "minecraft:wheat",
+    "minecraft:carrots",
+}
+function exists(array, predicate)
+    for _, v in ipairs(array) do
+        if predicate(v) then return true end
+    end
+    return false
+end
+function contains(array, item)
+    return exists(array, function (x) return x == item end)
+end
+
+local function isPlant(item)
+    return contains(plantNames, item)
+end
+
+local function isDig(item)
+    return isPlant(item) and 7 <= item.state.age
+end
+
+local function dig()
+    local ok, item = turtle.inspectDown()
+    if ok and isDig(item) then turtle.digDown() end
+end
+
+local function isFarm()
+    local ok, item = turtle.inspectDown()
+    if ok and isPlant(item) then return true end
+
+    if not ok then
+        if not findItem(function (i) return i.name:match("minecraft:[%w_]*hoe") end)
+        then
+            error("requires: hoe")
+        end
+
+        turtle.down()
+        local ok, item = turtle.inspectDown()
+        if ok and item.name == "minecraft:dirt" then
+            turtle.up()
+            turtle.equipRight()
+            return
+        end
+    end
+end
+
+local function isSeed(i) return contains(seedNames, i) end
+
+while isPlant() do
+    if not findItem(isSeed) then
+        error("requires: ", table.concat(seedNames, " or "))
+    end
+    dig()
+    selectItem(isSeed)
+    turtle.placeDown()
+    turtle.forward()
 end
