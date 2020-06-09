903f9c16951be675df266913b5ba7ff12a809c02
wiinuk <wii.nukegara@gmail.com>
2020-06-09 13:41:41 +0900

    appendArray を追加

--- sources/extensions.lua
+++ sources/extensions.lua
@@ -25,10 +25,27 @@ local function clamp(x, minBound, maxBound)
     return math.max(minBound, math.min(maxBound, x))
 end
 
+---@generic T
+---@param array1 table<integer, T>
+---@param array2 table<integer, T>
+---@return table<integer, T>
+local function appendArray(array1, array2)
+    if not array1 and not array2 then return nil end
+
+    array1 = array1 or {}
+    array2 = array2 or {}
+
+    local result = {}
+    for i = 1, #array1 do result[#result+1] = array1[i] end
+    for i = 1, #array2 do result[#result+1] = array2[i] end
+    return result
+end
+
 return {
     printError = printError,
     clearTable = clearTable,
     clearArray = clearArray,
+    appendArray = appendArray,
     noop = noop,
     clamp = clamp,
 }
