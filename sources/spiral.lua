9f6d5cdcc22a782adc859da0bb04847df73ba764
wiinuk <wii.nukegara@gmail.com>
2020-06-09 18:38:41 +0900

    spiral を追加

---
+++ sources/spiral.lua
@@ -0,0 +1,20 @@
+local refuel = require "refuel"
+
+local count = ({...})[1]
+
+for _ = 1, count do
+    turtle.dig()
+    turtle.turnLeft()
+    turtle.dig()
+    turtle.turnLeft()
+    turtle.dig()
+    turtle.turnLeft()
+    turtle.dig()
+    turtle.turnLeft()
+    turtle.dig()
+    while turtle.digUp() do end
+    refuel()
+    while turtle.up() do end
+end
+
+for _ = 1, count do turtle.down() end
