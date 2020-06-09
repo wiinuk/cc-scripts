8257613072504f413d8128230250bf9b29191643
wiinuk <wii.nukegara@gmail.com>
2020-06-09 22:04:21 +0900

    sync

--- sources/spiral.lua
+++ sources/spiral.lua
@@ -2,27 +2,33 @@ local refuel = require "refuel"
 
 local count = ({...})[1]
 
+local function digAround()
+    turtle.dig()
+    turtle.turnLeft()
+    turtle.dig()
+    turtle.turnLeft()
+    turtle.dig()
+    turtle.turnLeft()
+    turtle.dig()
+    turtle.turnLeft()
+    turtle.dig()
+end
 for _ = 1, count do
-    turtle.dig()
-    turtle.turnLeft()
-    turtle.dig()
-    turtle.turnLeft()
-    turtle.dig()
-    turtle.turnLeft()
-    turtle.dig()
-    turtle.turnLeft()
-    turtle.dig()
+    digAround()
     turtle.digUp()
     refuel()
     turtle.up()
 end
 
-for _ = 1, count do
-    refuel()
-    turtle.down()
-end
-
 for _ = 1, 2 do
+    turtle.dig()
     refuel()
     turtle.forward()
+end
+
+for _ = 1, count do
+    digAround()
+    turtle.digDown()
+    refuel()
+    turtle.down()
 end
