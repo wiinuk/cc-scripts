13b571c8af525192407e4391f47fa4c7c0da0070
wiinuk <wii.nukegara@gmail.com>
2020-06-10 12:08:39 +0900

    sync

---
+++ sources/fill-ri-triangle.lua
@@ -0,0 +1,59 @@
+local refuel = require "refuel"
+local size = ({...})[1]
+
+local height = size
+for _ = 1, size, 2 do
+    --[[
+        size = 3
+        [ ][ ][ ]
+        [ ][ ][ ]
+        [^][ ][ ]
+    ]]
+    turtle.placeDown()
+    for _ = 2, height do
+        refuel()
+        turtle.forward()
+        turtle.placeDown()
+    end
+    --[[
+        size = 3
+        [■^][ ][ ]
+        [■] [ ][ ]
+        [■] [ ][ ]
+    ]]
+    turtle.turnRight()
+    refuel()
+    turtle.forward()
+    turtle.turnRight()
+    refuel()
+    turtle.forward()
+    height = height - 1
+    --[[
+        size = 3
+        [■][ ][ ]
+        [■][v][ ]
+        [■][ ][ ]
+    ]]
+    turtle.placeDown()
+    for _ = 2, height do
+        refuel()
+        turtle.forward()
+        turtle.placeDown()
+    end
+    --[[
+        size = 3
+        [■][ ] [ ]
+        [■][■] [ ]
+        [■][■v][ ]
+    ]]
+    turtle.turnLeft()
+    refuel()
+    turtle.forward()
+    turtle.turnLeft()
+    --[[
+        size = 3
+        [■][ ][ ]
+        [■][■][ ]
+        [■][■][^]
+    ]]
+end
