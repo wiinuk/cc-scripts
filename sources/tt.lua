4725ba0042330697beb910189f52e2a993fdfd2b
wiinuk <wii.nukegara@gmail.com>
2020-06-12 13:06:39 +0900

    tt を追加

---
+++ sources/tt.lua
@@ -0,0 +1,87 @@
+local Rules = require "rules"
+local Logger = require "logger"
+local BasicRules = require "basic_rules"
+local ArgParser = require "arg-parser"
+local parseSubCommand = ArgParser.parseSubCommand
+local splitCommand = ArgParser.splitCommand
+
+Logger.addListener(Logger.fileWriterListener "/logs/tt.log")
+
+local mainLogger = Logger.create "tt-main-logger"
+mainLogger.addListener(Logger.printListener(Logger.Info))
+mainLogger.addListener(Logger.loggerListener(Logger.getDefaultLogger()))
+
+local function ruleThread()
+    Rules.add(
+        -- BasicRules.collectAroundMapRule,
+        -- BasicRules.craftTorchRule,
+        -- BasicRules.setTorchRule,
+        BasicRules.refuelRule,
+        BasicRules.goHomeRule
+    )
+    Rules.evaluate()
+end
+
+local history = {}
+local normalizedInputToCount = {}
+
+local function trim(s)
+    return s:match "^%s*(.-)%s*$"
+end
+
+---@param input string
+local function normalize(input)
+    local args = splitCommand(input)
+    if not args then return trim(input:lower()) end
+    return table.concat(args, " "):lower()
+end
+
+local function addHistory(input)
+    local key = normalize(input)
+    if normalizedInputToCount[key] then
+        for i = #history, 1, -1 do
+            local old = history[i]
+            if normalize(old) == key then
+                table.remove(history, i)
+            end
+        end
+        normalizedInputToCount[key] = normalizedInputToCount[key] + 1
+    else
+        normalizedInputToCount[key] = 1
+    end
+    history[#history+1] = input
+end
+local sumCommands = {}
+
+local function complete(prefix)
+    return shell.complete(prefix)
+end
+
+local function readAndProcessCommand()
+    local oldColor = term.getTextColor()
+    term.setTextColor(colors.lightBlue)
+    term.write("TT> ")
+    term.setTextColor(oldColor)
+
+    local input = read(nil, history, complete)
+    addHistory(input)
+
+    local arguments, error = splitCommand(input)
+    if not arguments then return mainLogger.logError("error: ", error) end
+
+    local sub, subArguments = parseSubCommand(arguments)
+    if not sub then return end
+
+    local subCommand = sumCommands[sub]
+    if subCommand then return subCommand(subArguments) end
+
+    shell.run(unpack(arguments))
+end
+
+local function inputThread()
+    while true do
+        readAndProcessCommand()
+    end
+end
+
+parallel.waitForAll(ruleThread, inputThread)
