761b270c48f550a4258a58e633778c715c89468e
wiinuk <wii.nukegara@gmail.com>
2020-06-14 11:22:59 +0900

    Vec3.multiply を追加

--- sources/vec3.lua
+++ sources/vec3.lua
@@ -3,6 +3,11 @@ local function manhattanDistance(ax, ay, az, bx, by, bz)
     return math.abs(ax - bx) + math.abs(ay - by) + math.abs(az - bz)
 end
 
+local function multiply(s, x, y, z)
+    return s * x, s * y, s * z
+end
+
 return {
     manhattanDistance = manhattanDistance,
+    multiply = multiply,
 }
