500439b892381549a2fa514b6bcd6f5e9425a7d3
wiinuk <wii.nukegara@gmail.com>
2020-12-03 09:57:59 +0900

    minecraft-names.lua を追加

---
+++ sources/libraries/minecraft-names.lua
@@ -0,0 +1,40 @@
+return {
+    Chest = "minecraft:chest",
+    Coal = "minecraft:coal",
+
+    Torch = "minecraft:torch",
+    CraftingTable = "minecraft:crafting_table",
+    Stick = "minecraft:stick",
+    Planks = "minecraft:planks",
+    Log = "minecraft:log",
+
+    Bucket = "minecraft:bucket",
+    FlowingWater = "minecraft:flowing_water",
+    FlowingLava = "minecraft:flowing_lava",
+    Lava = "minecraft:lava",
+    LavaBucket = "minecraft:lava_bucket",
+    Water = "minecraft:water",
+    WaterBucket = "minecraft:water_bucket",
+    CobbleStone = "minecraft:cobblestone",
+    Obsidian = "minecraft:obsidian",
+
+    Wheat = "minecraft:wheat",
+    Carrots = "minecraft:carrots",
+    Potatoes = "minecraft:potatoes",
+
+    WheatSeeds = "minecraft:wheat_seeds",
+    Carrot = "minecraft:carrot",
+    Potato = "minecraft:potato",
+
+    DiamondPickaxe = "minecraft:diamond_pickaxe",
+
+    Bedrock = "minecraft:bedrock",
+
+    Leaves = "minecraft:leaves",
+    Sapling = "minecraft:sapling",
+    Dye = "minecraft:dye",
+
+    StainedGlass = "minecraft:stained_glass",
+    Grass = "minecraft:grass",
+    Dirt = "minecraft:dirt",
+}
